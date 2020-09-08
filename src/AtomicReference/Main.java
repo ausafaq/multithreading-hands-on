4deb17b2e6a8925b972fda4759d7dfcb1b63d293
Ausaf Ahmed <ausaf.ahmed@RWC-WS118.local>
2020-09-07 22:56:38 -0700

    Experiment Atomic Reference and Lock Free Stack

---
+++ src/AtomicReference/Main.java
@@ -0,0 +1,18 @@
+package AtomicReference;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String oldName = "old name";
+        String newName = "new name";
+        AtomicReference<String> atomicReference = new AtomicReference<>(oldName);
+
+        if(atomicReference.compareAndSet(oldName, newName)) {
+            System.out.println("New value is " +atomicReference.get());
+        } else {
+            System.out.println("Nothing changed");
+        }
+    }
+}
